f1a928fb9dfa924a257635afd2770c1d9d380fb1
Siddharthm10 <siddharth.mehtaid@gmail.com>
2021-05-12 02:21:32 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# Vaccine Notifier 
+### Description
+This notifies you over email about the availiblity of vaccine in your area. It uses selenium and runs an automated browser in the background.
+ **Happy Vaccination** :)
+
+### Setup
+```pip install -r requirements.txt```
+
+### Configuration
+All changes are to be done [here](main.py) from line 85.
+Things to be changed are :
+- Phone Number: Please enter the registered phone number for easy login
+- Pin code: Please enter your pin code for checking the vaccine availiblity in your area
+- Sender's email address: Currently this project uses SMTP library, which requires a less secure google account. I would suggest you to google it up. It can be done easily. (PS. Make a new account/use extra account for this.)
+- Password: Enter the password of the less secure account here. For easy smtp login.(This is safe as this stays with you off the record)
+- Reciever's email address: This is a list of email addresses you wanna recieve notifications to.
+  
+### Run Script
+```
+git clone https://github.com/Siddharthm10/VaccineNotifier.git
+cd VaccineNotifier
+pip install -r requirements.txt
+python3 main.py
+```
+**NOTE** You will have to enter the otp once the program starts :)
+
+### All contribution are invited 
+I wish to make this code easy to use for everyone. All suggestions and contributions are invited :)
+Stay safe 
---
+++ main.py
@@ -0,0 +1,156 @@
+def checkIfAvailable(driver):
+    time.sleep(2)
+    global message
+    NameElementList = driver.find_elements_by_xpath('//*[@class="mat-main-field center-main-field"]/mat-selection-list/div[*]/mat-list-option/div/div[2]/ion-row/ion-col/div/h5')
+    StatusElementList = driver.find_elements_by_xpath('//*[@class="mat-main-field center-main-field"]/mat-selection-list/div[*]/mat-list-option/div/div[2]/ion-row/ion-col[2]/ul/li[2]/div/div/a')
+    statusList=[]
+    message = ""
+    # message += " [Vaccination Center Name]         [STATUS]\n"
+    for name,status in zip(NameElementList, StatusElementList):
+        message+= name.text + "  |  " + status.text + "\n"
+        statusList.append(status.text)
+
+    for status in statusList:
+        if(status!="NA" and status!="Booked" and status!=""):
+            return False
+    return True
+    
+
+
+def sendMail(sender_email, receiver_mails, password, message):
+    ######################## SMTP ###########################################
+    smtp_server = "smtp.gmail.com"
+    port = 587
+    # Create a secure SSL context
+    context = ssl.create_default_context()
+    ########################################################################
+    emailBody = f"""From: {sender_email} 
+        To: {receiver_email} 
+        Subject: Vaccination Info
+        """ + message
+    emailBody = f"""From: Vaccine Notification System <{sender_email}>
+        To: Your given mail ID <{receiver_email}>
+        MIME-Version: 1.0
+        Content-type: text/html
+        Subject: Vaccine Notification
+
+        The information regarding the current slot status is:
+
+        Vaccination Center    |    Status 
+        {message}
+    """
+
+    try:
+        server = smtplib.SMTP(smtp_server,port)
+        server.ehlo() # Can be omitted
+        server.starttls(context=context) # Secure the connection
+        server.ehlo() # Can be omitted
+        server.login(sender_email, password)
+        print(emailBody)
+        server.sendmail(sender_email, receiver_mails, emailBody)
+    except :
+        print("error in sending the mail")
+    finally:
+        server.quit
+
+
+if __name__=="__main__":
+    import time
+    import sys
+    import smtplib, ssl
+    from selenium import webdriver
+    from selenium.webdriver.common.keys import Keys
+    from selenium.webdriver.chrome.options import Options
+    from selenium.webdriver.common.by import By
+    # from selenium.webdriver.firefox.webdriver import WebDriver
+    from selenium.webdriver.support.ui import WebDriverWait
+    from selenium.webdriver.support import expected_conditions as EC
+
+
+    ############# SELENIUM CONFIG- BRAVE#############################
+    PATH = "/home/siddharthm10/Documents/dump/chromedriver"
+    options = Options()
+    ##Change this to your current browser :)
+    options.binary_location = "/usr/bin/brave-browser-stable"
+    driver = webdriver.Chrome(options = options, executable_path= PATH)
+    ###############################################################
+
+    ############## DONT CHANGE THIS################################
+    driver.get("https://selfregistration.cowin.gov.in/appointment")
+    print(driver.title)
+    message = ""
+    ###############################################################
+
+
+    ################USER CONFIG###################################
+    PHONE = "123456789"
+    PINCODE = "133456"
+    sender_email = "abc@gmail.com"
+    password = "pass123"
+    receiver_email = ["email1@gmail.com", "email2@gmail.com"]
+    ##############################################################
+
+    driver.refresh()
+    phoneNo = driver.find_element_by_xpath('//*[@id="mat-input-0"]')
+    time.sleep(2)
+    phoneNo.send_keys(PHONE)
+    getOtp = driver.find_element_by_xpath('//*[@class="register-box register-main-box ng-star-inserted md hydrated"]/ion-grid/ion-row/ion-col/ion-grid/form/ion-row/ion-col[2]/div/ion-button')
+    getOtp.click()
+    try:
+        #Schedule1
+        schedule1 = WebDriverWait(driver, 3000).until(
+            EC.presence_of_element_located((By.XPATH, '//*[@class="beneficiary-box md hydrated"]/ion-row[2]/ion-col/ion-grid/ion-row[4]/ion-col[2]/ul/li/a'))
+        )
+        time.sleep(2)
+        schedule1.click()
+
+        #Schedule2
+        schedule2 = WebDriverWait(driver, 300).until(
+            EC.presence_of_element_located((By.XPATH, '//*[@id="main-content"]/app-beneficiary-dashboard/ion-content/div/div/ion-grid/ion-row/ion-col/ion-grid[1]/ion-row[4]/ion-col/div/div[2]/div/ion-button'))
+        )
+        time.sleep(2)
+        schedule2.click()
+
+        #pinCode
+        pinCode = WebDriverWait(driver, 300).until(
+            EC.presence_of_element_located((By.XPATH, '//*[@id="mat-input-2"]'))
+        )
+        time.sleep(2)
+        pinCode.send_keys(PINCODE)
+        
+        #Search
+        SearchBtn = WebDriverWait(driver, 300).until(
+            EC.presence_of_element_located((By.XPATH, '//*[@class="register-wrap"]/ion-grid/ion-row/ion-grid/ion-row/ion-col/ion-grid/ion-row/ion-col[2]/form/ion-grid/ion-row/ion-col[3]/ion-button'))
+        )
+        time.sleep(2)
+        SearchBtn.click()
+
+        #18+
+        filter18 = WebDriverWait(driver, 300).until(
+            EC.presence_of_element_located((By.XPATH, '//*[@class="padding-0 ng-star-inserted md hydrated"]/ion-row/ion-col/ion-grid/ion-row/ion-col[2]/form/ion-grid/ion-row/ion-col[5]/div/div/label'))
+        )
+        time.sleep(2)
+        filter18.click()
+
+        #Check if available
+        while(checkIfAvailable(driver)):
+            
+            time.sleep(3)
+            button = WebDriverWait(driver, 300).until(
+                EC.presence_of_element_located((By.XPATH, '//*[@class="register-wrap"]/ion-grid/ion-row/ion-grid/ion-row/ion-col/ion-grid/ion-row/ion-col[2]/form/ion-grid/ion-row/ion-col[3]/ion-button'))
+            )
+            button.click()
+            filter18 = WebDriverWait(driver, 300).until(
+                EC.presence_of_element_located((By.XPATH, '//*[@class="padding-0 ng-star-inserted md hydrated"]/ion-row/ion-col/ion-grid/ion-row/ion-col[2]/form/ion-grid/ion-row/ion-col[5]/div/div/label'))
+            )
+            time.sleep(2)
+            filter18.click()
+
+        # Trigger action
+        # print(message)
+        sendMail(sender_email, receiver_email, password, message)
+
+    finally:
+        driver.quit()
+
+
